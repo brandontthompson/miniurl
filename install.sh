cbbc01bf473fd43b9876271324c3d61fc245d0f6
brandon thompson <XhailOrigin@gmail.com>
2021-08-15 15:38:55 -0700

    install.sh

--- install.sh
+++ install.sh
@@ -19,4 +19,4 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 sudo docker build -t uwaamoe/node .
 
 sudo docker run --name uwaamoe-database -v ~/database:/data/db -p 27017:27017 -d mongo:latest
-sudo docker run -p 443:5231 --name uwaamoe-app -d uwaamoe/node
+sudo docker run --name uwaamoe-app -p 443:5231 -d uwaamoe/node
